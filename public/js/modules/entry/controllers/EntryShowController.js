1636bae41303be35223b1a8aa5a40353a0b7a6b7
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-01-01 20:57:07 +0100

    Entry detail view improvement, FlashController

--- public/js/modules/entry/controllers/EntryShowController.js
+++ public/js/modules/entry/controllers/EntryShowController.js
@@ -1,87 +1,94 @@
 angular.module('IOUApp')
 
-    .controller('EntryShowController', ['$scope', "$routeParams", 'EntryFactory', 'AuthFactory',
-    function($scope, $routeParams, EntryFactory, AuthFactory) {
+    .controller('EntryShowController',
+    function($scope, $routeParams, $location, EntryFactory, AuthFactory) {
         $scope.userData = AuthFactory.getUserData();
         $scope.entry = {};
 
         var fetchEntry = function() {
+
             EntryFactory.getOne($routeParams.id)
                 .success(function(entry) {
                     $scope.entry = entry;
+
+                    $scope.isOpen = isOpen();
+
+                    $scope.isAccepted = isAccepted();
+
+                    $scope.isRejected = isRejected();
+
+                    $scope.isDeleted = isDeleted();
+
+                    $scope.isEditable = isEditable();
+
+                    $scope.isAcceptable = isAcceptable();
+
+                    $scope.isRejectable = isRejectable();
+
+                    $scope.isDeletable = isDeletable();
+
+                    $scope.isDebtor = isDebtor();
+
+                    $scope.isLender = isLender();
                 });
-        }
+        };
+
+        fetchEntry();
 
         var getStatus = function(statusId) {
             return EntryFactory.getStatus(statusId);
-        }
+        };
 
         var isOpen = function() {
             return $scope.entry.status === 0;
-        }
+        };
 
         var isAccepted = function() {
             return $scope.entry.status === 1;
-        }
+        };
 
         var isRejected = function() {
             return $scope.entry.status === 2;
-        }
+        };
 
         var isDeleted = function() {
             return $scope.entry.status === 3;
-        }
+        } ;
 
         var isEditable = function() {
             return (isOpen() || isRejected());
-        }
+        };
 
         var isAcceptable = function() {
             return (isOpen() || isRejected());
-        }
+        };
 
-        var isRejectable = function(entry) {
+        var isRejectable = function() {
             return (!isAccepted() || isRejected() || isDeleted());
-        }
+        };
 
-        var isDeletable = function(entry) {
+        var isDeletable = function() {
             return (!isAccepted() || isDeleted());
-        }
+        };
 
         var isDebtor = function() {
-            console.log('debtor_id', $scope.entry.debtor_id);
-            console.log('user_id', $scope.userData.ioweyouId);
-            return  $scope.userData.ioweyouId === $scope.entry.debtor_id;
-        }
+            return  $scope.userData.ioweyouId == $scope.entry.debtor_id;
+        };
 
         var isLender = function() {
-            return  $scope.userData.ioweyouId === $scope.entry.lender_id;
-        }
+            return  $scope.userData.ioweyouId == $scope.entry.lender_id;
+        };
 
-        $scope.fetchEntry = fetchEntry;
+        var deleteEntry = function() {
+            EntryFactory.delete($scope.entry.id)
+                .success(function(){
+                    $scope.$emit('flashMessage', 'success', 'Entry deleted successfully.');
+                    $location.path('/entry/list');
+                });
+        };
 
         $scope.getStatus = getStatus;
 
-        $scope.isOpen = isOpen;
-
-        $scope.isAccepted = isAccepted;
-
-        $scope.isRejected = isRejected;
-
-        $scope.isDeleted = isDeleted;
-
-        $scope.isEditable = isEditable;
-
-        $scope.isAcceptable = isAcceptable;
-
-        $scope.isRejectable = isRejectable;
-
-        $scope.isDeletable = isDeletable;
-
-        $scope.isDebtor = isDebtor;
-
-        $scope.isLender = isLender;
-
-        $scope.fetchEntry();
+        $scope.deleteEntry = deleteEntry;
     }
-]);
+);
